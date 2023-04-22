65785a0a2537bb6fd26dd2df93ea258f9bb3ca60
SoaresLeonardo <leonardohenriquesoarasalvez784@gmail.com>
2023-04-22 11:55:51 -0300

    Update index.ts

--- src/utils/LocalStorage/index.ts
+++ src/utils/LocalStorage/index.ts
@@ -1,20 +1,12 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 export function getStorageItem(key: string) {
-  if (typeof window === 'undefined') return;
-
-  const data = window.localStorage.getItem(
-    `${process.env.NEXT_APP_KEY_PUBLIC}_${key}`
-  );
+  const data = window.localStorage.getItem(key);
 
   return JSON.parse(data!);
 }
 
 export function setStorageItem(key: string, value: unknown) {
-  if (typeof window === 'undefined') return;
-
   const data = JSON.stringify(value);
 
-  return window.localStorage.setItem(
-    `${process.env.NEXT_APP_KEY_PUBLIC}_${key}`,
-    data
-  );
+  return window.localStorage.setItem(key, data);
 }
