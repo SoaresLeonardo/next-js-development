1920379d828ed10fc34b399bf65d854627f9e997
SoaresLeonardo <leonardohenriquesoarasalvez784@gmail.com>
2023-05-12 16:19:02 -0300

    CartPage

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import CartItem from '@/components/CartItem';
+import { RootState } from '@/redux/cart/types';
+import { useSelector } from 'react-redux';
+
+import * as C from '@/components/Container';
+import { selectProductsTotalPrice } from '@/redux/cart/selectors';
+
+export default function Cart() {
+  const { products } = useSelector(
+    (rootReducer: RootState) => rootReducer.cartReducer
+  );
+
+  const productsTotalPrice = useSelector(selectProductsTotalPrice);
+
+  return (
+    <div>
+      <h1>Carrinho</h1>
+      {products.length > 0 && (
+        <C.Container backgroundColor>
+          {products &&
+            products.map((product) => (
+              <CartItem product={product} key={product.id} />
+            ))}
+
+          <span>{`Preço total: R$${productsTotalPrice}`}</span>
+        </C.Container>
+      )}
+    </div>
+  );
+}
