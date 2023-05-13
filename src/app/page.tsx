ad0279402ea918e7daaea50bc3b3d6c151b8a3a8
SoaresLeonardo <leonardohenriquesoarasalvez784@gmail.com>
2023-05-12 21:25:42 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,7 @@
-'use client';
-
-import Button from '@/components/Button';
-import { useDispatch, useSelector } from 'react-redux';
-import * as C from '@/components/Container';
-import { decrement, increment } from '@/redux/counter';
-
 export default function Home() {
-  const counter = useSelector((state) => state.counter.value);
-  const dispatch = useDispatch();
-
-  const handleIncrementCounter = () => {
-    dispatch(increment());
-  };
-
-  const handleDecrementCounter = () => {
-    dispatch(decrement());
-  };
-
   return (
     <>
-      <C.Container backgroundColor>
-        <h1>Counter {counter}</h1>
-        <Button onClick={handleIncrementCounter}>Incrementar</Button>
-        <Button onClick={handleDecrementCounter}>Decrementar</Button>
-      </C.Container>
+      <h1>Home</h1>
     </>
   );
 }
