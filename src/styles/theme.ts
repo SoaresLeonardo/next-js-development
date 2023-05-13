2ced0bd5d56ed312f5a0f8b15c9b60149015e978
SoaresLeonardo <leonardohenriquesoarasalvez784@gmail.com>
2023-05-13 13:52:54 -0300

    Create theme.ts

---
+++ src/styles/theme.ts
@@ -0,0 +1,58 @@
+export const theme = {
+  border: {
+    radius: '0.4rem'
+  },
+  box: {
+    shadow: '0 8px 8px rgba(0, 0, 0, 0.2)'
+  },
+  colors: {
+    primary: '#24A66B',
+    secundary: '#5855E9',
+    white: '#FAFAFA',
+    gray_50: '#f0efeb',
+    gray_100: '#D9D9D9',
+    gray_200: '#91AAB4',
+    gray_300: '#3E606F',
+    gray_450: '#193441',
+    gray_500: '#343a40',
+    yellow_300: '#F1C40F',
+    purple_100: '#7F57DF',
+    purple_300: '#5855E9',
+    green: '#24A66B',
+    red: '#E63737',
+    success: '#24A66B',
+    error: '#E63737',
+    info: '#189CEF',
+    black: '#0D0D0D'
+  },
+  font: {
+    family:
+      "Montserrat, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+    light: 300,
+    normal: 400,
+    bold: 600,
+    sizes: {
+      xxxxsmall: '0.8rem',
+      xxxsmall: '1.2rem',
+      xxsmall: '1.4rem',
+      xsmall: '1.6rem',
+      small: '2rem',
+      medium: '2.4rem',
+      large: '3.2rem',
+      xlarge: '4rem',
+      xxlarge: '4.8rem',
+      xxxlarge: '5.6rem',
+      huge: '6.4rem'
+    }
+  },
+  spacings: {
+    xxxsmall: '0.8rem',
+    xxsmall: '1rem',
+    xsmall: '1.6rem',
+    small: '2.4rem',
+    medium: '3.2rem',
+    large: '4.0rem',
+    xlarge: '4.8rem',
+    xxlarge: '5.6rem'
+  }
+};
