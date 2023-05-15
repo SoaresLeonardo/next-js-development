2f4d91db7355cf0d37cfbffc2f21602162137ad7
SoaresLeonardo <leonardohenriquesoarasalvez784@gmail.com>
2023-05-14 21:01:52 -0300

    Page products

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -5,13 +5,24 @@ import { AiOutlineShoppingCart } from 'react-icons/ai';
 import Products from '@/components/Products';
 
 import Header from '@/components/Header';
+import { useSelector } from 'react-redux';
+import { State } from '@/redux/products/types';
+import Link from 'next/link';
+
 export default function ProductsPage() {
+  const products = useSelector((state: State) => state.products);
+
+  console.log(products);
   return (
     <>
       <Header
         title="Shopping cart"
         hasLogin
-        icon={() => <AiOutlineShoppingCart />}
+        icon={() => (
+          <Link href="cart">
+            <AiOutlineShoppingCart />
+          </Link>
+        )}
       />
       <Products />
     </>
