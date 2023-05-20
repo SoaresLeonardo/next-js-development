8c4458372c24987b8398b5a7a67705f3a77387e9
SoaresLeonardo <leonardohenriquesoarasalvez784@gmail.com>
2023-05-20 20:43:31 -0300

    Update index.tsx

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -1,18 +1,11 @@
-import { useSelector } from 'react-redux';
 import { State } from '@/redux/products/types';
 import { CartItem } from '../CartItem';
-import Link from 'next/link';
 
-const Cart = () => {
-  const products = useSelector((state: State) => state.products);
-
+const Cart = ({ products }: State) => {
   if (products.length < 1) {
     return (
       <>
         <h1>Ops! não a nada aqui.</h1>
-        <p>
-          volte para a <Link href="/">home</Link>
-        </p>
       </>
     );
   }
