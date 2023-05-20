e567d2a97e1b430191ef528ed3fbbb8e44a1e8fe
SoaresLeonardo <leonardohenriquesoarasalvez784@gmail.com>
2023-05-20 20:42:42 -0300

    Cart Page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -3,12 +3,16 @@
 import Cart from '@/components/Cart';
 
 import * as S from '@/components/Container';
+import { State } from '@/redux/products/types';
+import { useSelector } from 'react-redux';
 
 export default function CartPage() {
+  const { products } = useSelector((state: State) => state);
+  console.log(products);
   return (
     <S.Container>
       <h1>Meu carrinho</h1>
-      <Cart />
+      <Cart products={products} />
     </S.Container>
   );
 }
