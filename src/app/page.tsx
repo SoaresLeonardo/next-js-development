da64450da716ff76e4c6b146b9e96f6eeec47350
SoaresLeonardo <leonardohenriquesoarasalvez784@gmail.com>
2023-05-20 20:42:58 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,38 +2,19 @@
 
 import * as S from '@/components/Container';
 import Header from '@/components/Header';
-import { useDispatch, useSelector } from 'react-redux';
+import Login from '@/components/Login';
+import { StateProps } from '@/redux/user';
+import { useSelector } from 'react-redux';
 
 export default function Home() {
-  const user = useSelector((state) => state.currentUser);
-  console.log(user);
-
-  const dispatch = useDispatch();
-
-  const handleUserLogin = () => {
-    dispatch({
-      type: 'user/login',
-      payload: { name: 'Leonardo', email: 'leonardo@gmail.com' }
-    });
-  };
-
-  const handleUserLogout = () => {
-    dispatch({
-      type: 'user/logout'
-    });
-  };
+  const user = useSelector((state: StateProps) => state.currentUser);
 
   return (
     <>
-      <Header
-        title="Header"
-        hasLogin
-        textLoginAction={user ? 'Logout' : 'Login'}
-        loginAction={user ? handleUserLogout : handleUserLogin}
-      />
+      <Header title="Header" login={<Login currentUser={user} />} />
       <S.Container>
         <h1>User</h1>
-        {user && <p>Olá {user.name}</p>}
+        {/* {user && <p>Olá {user.name}</p>} */}
       </S.Container>
     </>
   );
