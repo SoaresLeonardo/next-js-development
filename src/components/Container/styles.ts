f8c59379736339340baebd171297c689964bc3a5
SoaresLeonardo <leonardohenriquesoarasalvez784@gmail.com>
2023-05-27 18:37:14 -0300

    Update styles.ts

--- src/components/Container/styles.ts
+++ src/components/Container/styles.ts
@@ -1,33 +1,35 @@
 import styled, { css } from 'styled-components';
 
 export const Container = styled.div`
-  h1 {
-    text-align: center;
-  }
-
   ${({ theme }) => css`
- max-width: 28rem;
-  background-color: ${theme.colors.white};
-  margin: 50px auto;
-  padding: ${theme.spacings.xxxsmall};
-  gap: ${theme.spacings.xxxsmall};
-  flex-direction: column;
-  display: flex;
-  box-shadow: ${theme.box.shadow};
-  border-radius: ${theme.border.radius};
-
-  button {
-    /* color: ${theme.colors.white}; */
-    /* padding: ${theme.spacings.xxxsmall};
-    background-color: ${theme.colors.purple_100};
-
-    color: ${theme.colors.white};
+    max-width: 28rem;
+    background-color: ${theme.colors.white};
+    margin: 50px auto;
+    padding: ${theme.spacings.xxxsmall};
+    gap: ${theme.spacings.xxxsmall};
+    flex-direction: column;
+    display: flex;
+    box-shadow: ${theme.box.shadow};
     border-radius: ${theme.border.radius};
-    width: 100%;
-    transition: 0.1s;
 
-    &:hover {
-      background-color: ${theme.colors.secundary};
-    } */
- `}
+    button {
+      color: ${theme.colors.white};
+      padding: ${theme.spacings.xxxsmall};
+      background-color: ${theme.colors.purple_100};
+
+      color: ${theme.colors.white};
+      border-radius: ${theme.border.radius};
+      width: 100%;
+      transition: 0.1s;
+    }
+
+    h1 {
+      text-align: center;
+    }
+
+    form {
+      display: grid;
+      gap: 10px;
+    }
+  `}
 `;
